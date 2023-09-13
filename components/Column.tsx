84b333d6ed119bb441932185887479b7966d4a56
dhavalgeek <dhavalmgeek435@gmail.com>
2023-09-13 17:00:16 +0530

    drag drop with update and search

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,5 +1,8 @@
+import { PlusCircleIcon } from '@heroicons/react/24/solid'
 import React from 'react'
 import { Draggable, Droppable } from 'react-beautiful-dnd'
+import TodoCard from './TodoCard'
+import { useBoardStore } from '@/store/BoardStore'
 
 type Props = {
 	id: TypedColumn,
@@ -7,7 +10,17 @@ type Props = {
 	index: number
 }
 
+const idToColumnText: {
+	[key in TypedColumn]: string;
+} = {
+	"todo": "To Do",
+	"inprogress": "In Progress",
+	"done": "Done"
+}
+
 function Column({ id, todos, index }: Props) {
+	const [searchString] = useBoardStore(state => [state.searchString]);
+
 	return (
 		<Draggable draggableId={id} index={index}>
 			{(provided) => (
@@ -23,7 +36,44 @@ function Column({ id, todos, index }: Props) {
 								ref={provided.innerRef}
 								className={`pb-2 p-2 rounded-2xl shadow-sm ${snapshot.isDraggingOver ? 'bg-green-200' : 'bg-white/50'}`}
 							>
+								<h2 className='flex justify-between font-bold text-xl p-2'>
+									{idToColumnText[id]}
+									<span className='text-gray-500 bg-gray-200 rounded-full px-2 py-1 text-small font-normal'>
+										{!searchString ? todos.length : todos.filter(todo => todo.title.toLowerCase().includes(searchString.toLocaleLowerCase())).length}
+									</span>
+								</h2>
 
+								<div className="space-y-2">
+									{todos.map((todo, index) => {
+										if (searchString && !todo.title.toLowerCase().includes(searchString.toLocaleLowerCase())) return null;
+
+										return (
+											<Draggable
+												key={todo.$id}
+												index={index}
+												draggableId={todo.$id}
+											>
+												{(provided) => (
+													<TodoCard
+														todo={todo}
+														index={index}
+														id={id}
+														innerRef={provided.innerRef}
+														draggableProps={provided.draggableProps}
+														dragHandleProps={provided.dragHandleProps}
+													/>
+												)}
+											</Draggable>
+										);
+									})}
+									<div className='flex items-end justify-end p-2'>
+										<button className='text-green-500 hover:text-green-600'>
+											<PlusCircleIcon
+												className='h-10 w-10'
+											/>
+										</button>
+									</div>
+								</div>
 							</div>
 						)}
 					</Droppable>
